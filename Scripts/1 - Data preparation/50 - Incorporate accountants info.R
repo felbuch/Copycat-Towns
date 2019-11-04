954ecf78ae12554b8e92be3fdf95c0571432f502
felbuch <felbuch@gmail.com>
2019-11-04 16:34:07 -0300

    Create script to incorporte accountants info

---
+++ Scripts/1 - Data preparation/50 - Incorporate accountants info.R
@@ -0,0 +1,39 @@
+
+
+library(magrittr)
+library(data.table)
+library(zoo)
+
+
+setwd(project_folder)
+
+#Define the number of sheets in the Excel workbook file containing information data
+#(It's 26, but I didn't want just to hard code it.)
+nb_sheets <- readxl::excel_sheets("./Out-of-git/1 - Raw data/Accountants.xlsx") %>% length()
+
+#Create list to store the datatables for each sheet
+accountants_list_of_sheets <- list()
+
+#Loop over all sheets in the Excel file and store them as elements in the list
+for(sheet_nb in 1:nb_sheets){
+  accountants <- readxl::read_xlsx("./Out-of-git/1 - Raw data/Accountants.xlsx", sheet = sheet_nb) %>% as.data.table()
+  accountants_list_of_sheets[[sheet_nb]] <- accountants
+}
+
+#Concatenate all elements of list into a single data table with info on accountants
+accountants <- do.call(rbind, accountants_list_of_sheets)
+rm(accountants_list_of_sheets, nb_sheets, sheet_nb)
+
+#Translate names to English
+names(accountants) <- c("municipality","municipality_id","mayor","secretary","accountant")
+accountants %>% is.data.table #Checkpoint of format
+
+#The Excel file had merged tables.
+#We now wrangle the data to put it into a formal dataset
+#We use the accountant's e-mail as a way to identify the accountant
+accountants <- na.locf(accountants)
+accountants %<>% .[accountant %like% "e-mail:", 
+                   .(municipality = unique(municipality), 
+                   accountant = unique(accountant)),
+                   municipality_id]
+

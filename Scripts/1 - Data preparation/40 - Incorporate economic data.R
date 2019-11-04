ad8f8de6064e67c8b2b31720ee5237150c93506d
felbuch <felbuch@gmail.com>
2019-11-04 00:17:36 -0300

    Add script to incorporate economic data, namely GDP and urban hierarchy to the dataset

---
+++ Scripts/1 - Data preparation/40 - Incorporate economic data.R
@@ -0,0 +1,45 @@
+#Set main working directory
+project_folder <- "C:/Users/Felipe/Desktop/Duke MIDS/Modelling and Representation of Data/0 - Final Project/"
+setwd(project_folder)
+
+#Load economic data
+setwd("../0 - Final Project/Out-of-git/1 - Raw data/")
+econ = readxl::read_xlsx("./Brazil Municipalities GDP.xlsx") %>% as.data.table
+
+#Filter latest year (2016)
+econ <- econ[Ano == max(unique(econ$Ano))]
+
+#Select columns of interest
+econ %<>% .[,.(municipality_id = `Código do Município`,
+              gdp = `Produto Interno Bruto, a preços correntes\r\n(R$ 1.000)`,
+              gdp_per_capita = `Produto Interno Bruto per capita\r\n(R$ 1,00)`,
+              urban_hierarchy = `Hierarquia Urbana (principais categorias)`
+              )]
+
+#Define variable types
+econ$municipality_id %<>% as.numeric
+econ$gdp %<>% as.numeric
+econ$gdp_per_capita %<>% as.numeric
+econ$urban_hierarchy %<>% as.factor
+
+#Set order of levels for urban hierarchy
+hierarchy_order <- c("Centro Local",
+                     "Centro de Zona",
+                     "Centro Subregional",
+                     "Centro Regional",
+                     "Metrópole")
+
+levels(econ$urban_hierarchy) <- hierarchy_order
+
+levels(econ$urban_hierarchy) <- c("Local Center",
+                                  "Zone Center",
+                                  "Subregional Center",
+                                  "Regional Center",
+                                  "Metropolis")
+
+#Merge with cities dataset
+cities <- dplyr::inner_join(cities, econ, by = "municipality_id")
+
+setwd(project_folder)
+setwd("./Copycat-Towns/Datasets/2 - Intermediary data/")
+save(cities, file = "Cities_with_Covariates.RData")
